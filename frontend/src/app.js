314867fb92bcf3f1bdbb87595e4b6937f5e099da
JenniKate Wallace <jennikate@gmail.com>
2020-05-15 12:38:04 +0100

    Build

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,34 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import 'bulma/css/bulma.css'
 import './styles/main.scss'
 
-// import Header from './components/Header'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import MonsterList from './pages/MonsterList'
-import SingleMonster from './pages/SingleMonster'
-import EncounterGenerator from './pages/EncounterGenerator'
+import Home from './components/pages/Home'
 
 const App = () => {
   return (
     <>
-      <HashRouter>
-        {/* <Header /> */}
-
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/monsters' component={MonsterList} />
-          <Route exact path='/monsters/:slug' component={SingleMonster} />
-          <Route exact path='/encounter/generator' component={EncounterGenerator} />
-
-        </Switch>
-      </HashRouter>
+      <BrowserRouter><Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+      </BrowserRouter>
     </>
   )
 }
