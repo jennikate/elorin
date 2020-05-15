563176b0b475e8d467e337d7f63a8cae52e6e9ee
JenniKate Wallace <jennikate@gmail.com>
2020-05-15 12:02:10 +0100

    Remove jwt

--- jwt_auth/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class User(AbstractUser):
-
-    email = models.CharField(max_length=50, unique=True)
-    # profile_image = models.CharField(max_length=500)
